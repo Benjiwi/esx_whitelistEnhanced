5144c0a004dd34707c44e495e7098e9ada5d9f03
Benjiwi <50799557+Benjiwi@users.noreply.github.com>
2019-05-25 05:54:27 -0400

    HEEEHEHEE

--- locales/en.lua
+++ locales/en.lua
@@ -1,23 +1,23 @@
 Locales['en'] = {
-  ['not_in_whitelist'] = 'This server is using a whitelist. To talk about it, join us here : %s',
-  ['missing_steam_id'] = 'Unable to find your steamID',
-  ['banned_from_server'] = 'You are banned from this server',
-  ['waiting_queue_message'] = 'You are in the waiting queue (%i/%i)',
-  ['waiting_free_priority_slots'] = 'Waiting for a priority slot (%i ppriority slots, estimated waiting %im %is)',
+  ['not_in_whitelist'] = 'Este servidor esta utilizando una whitelist. Para hablar de ello, unete a nosotros aqui : %s',
+  ['missing_steam_id'] = 'Incapaz de encontrar tu steamID',
+  ['banned_from_server'] = 'Has sido baneado',
+  ['waiting_queue_message'] = 'Estas en la cola de espera (%i/%i)',
+  ['waiting_free_priority_slots'] = 'Esperando un Slot prioritario (%i ppriority slots, tiempo esperado %im %is)',
 
-  ['log_trying_to_connect'] = '%s [%s] is trying to connect',
-  ['log_missing_steam_id'] = 'Player %s doesn\'t have a steamID',
-  ['log_banned_from_server'] = '%s [%s] is banned : \'%s\'',
-  ['log_not_in_whitelist'] = '%s [%s] is not in whitelist',
-  ['log_already_in_priority_queue'] = '%s [%s] is already in priority queue',
-  ['log_added_to_priority_queue'] = '%s [%s] was added to priority queue',
-  ['log_removed_from_priority_queue'] = '%s [%s] was removed from priority queue',
-  ['log_player_connected_as_vip'] = '%s logged in as VIP',
+  ['log_trying_to_connect'] = '%s [%s] intentado conectar',
+  ['log_missing_steam_id'] = 'Jugador %s no tiene un steamID',
+  ['log_banned_from_server'] = '%s [%s] es baneado : \'%s\'',
+  ['log_not_in_whitelist'] = '%s [%s] no esta en whitelist',
+  ['log_already_in_priority_queue'] = '%s [%s] esta en cola de prioridad',
+  ['log_added_to_priority_queue'] = '%s [%s] se agrego a la cola de prioridad',
+  ['log_removed_from_priority_queue'] = '%s [%s] eliminado de la cola de prioridad',
+  ['log_player_connected_as_vip'] = '%s inicia sesion como VIP',
 
-  ['log_started_anti_spam'] = 'Anti-spam system started for %s',
-  ['anti_spam_message'] = 'Anti-spam protection: please wait %i seconds, you will be connected automatically',
-  ['log_stopped_anti_spam'] = 'Anti-spam system stopped for %s',
+  ['log_started_anti_spam'] = 'Sistema Anti-Spam iniciado para %s',
+  ['anti_spam_message'] = 'Proteccion Anti-Spam: por favor espere %i segundos, seras conectado automaticamente',
+  ['log_stopped_anti_spam'] = 'Sistema Anti-Spam detenido for %s',
 
-  ['reload_whitelist'] = 'Reload whitelist',
-  ['whitelist_reloaded'] = 'Successfully reloaded whitelist'
+  ['reload_whitelist'] = 'Actualizar whitelist',
+  ['whitelist_reloaded'] = 'Whitelist exitosamente actualizada'
 }
